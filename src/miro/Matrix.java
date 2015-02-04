fc7ce36e1d426b8ffdf6caa9d35e01e6126f477b
MiroslavKocian <mirkocian@gmail.com>
2015-02-04 22:13:49 +0100

    last version simpified all

--- src/miro/Matrix.java
+++ src/miro/Matrix.java
@@ -2,15 +2,11 @@ package miro;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Matrix {
     
@@ -26,45 +22,7 @@ public class Matrix {
 		return data;
 	}
 
-	public static Matrix fromString(String matrixSpecification) {
-	    List<String> rows = toRows(matrixSpecification);
-		List<List<String>> stringData = toStringMatrix(rows);
-		List<String> intData = concatenateRows(stringData);
-
-		return new Matrix(intData); 
-	}
-	
-	static List<String> toRows(String matrixSpecification) {
-        List<String> rows = Arrays.asList(matrixSpecification.split("\n"));     
-        return rows;
-    }
-	
-	private static List<List<String>> toStringMatrix(List<String> rows) {  
-        List<List<String>> stringData = new ArrayList<List<String>>();
-
-        for(String row : rows) {
-            stringData.add(Arrays.asList(row.split(",")));
-        }
-        return stringData;
-    }
-	
-	static List<String> concatenateRows(List<List<String>> stringData) {
-		ArrayList<String> intData = new ArrayList<String>();
-		
-		for(List<String> row : stringData) {
-			String integerRow = "";
-			
-			for(String value : row) {
-				integerRow += value;
-			}
-			
-			intData.add(integerRow);
-		}
-		return intData;
-	}
-	
-
-	public List<Integer> getMaxSlice() {
+	public List<Integer> getRowsWithMaxSlice() {
         ArrayList<Integer> rowsWithMaxSlice = new ArrayList<Integer>();
         for (Entry<Integer, Integer> rowEntry : getMaxSlicesByRow().entrySet()) {           
             if (rowEntry.getValue() == getTotalMaxSliceSize()) {
@@ -72,6 +30,13 @@ public class Matrix {
             }   
         }
         return rowsWithMaxSlice;
+    }
+	
+	public static Matrix fromString(String matrixSpecification) {
+        matrixSpecification = matrixSpecification.replace(",", "");
+        List<String> matrixRows = Arrays.asList(matrixSpecification.split("\n"));
+
+        return new Matrix(matrixRows);
     }
 
 	private int getTotalMaxSliceSize() {
--- src/miro/MatrixTest.java
+++ src/miro/MatrixTest.java
@@ -27,31 +27,11 @@ public class MatrixTest extends TestCase {
         
         assertEquals(data, matrix.getData());
     }
-    public void testToRow() {
-        List<String> matrixSpecification = Arrays.asList("1,0,0","1,1,0");
-        List<String> rows = Arrays.asList("1,0,0\n1,1,0".split("\n"));    
-        assertEquals(matrixSpecification, rows);
-    }
-    
-    public void testToRow2() {
-        String fa = ("1,0,0\n1,1,0");
-        List<String> rows = Matrix.toRows(fa);
-        assertEquals(Arrays.asList("1,0,0","1,1,0"), rows);
-    }
-    
-    public void testConcatenateRows() {
-        List<List<String>> stringData = new ArrayList<List<String>>();
-        stringData.add(Arrays.asList("1","0","0"));
-        stringData.add(Arrays.asList("1","1","0"));
-        List<String> concRows = Matrix.concatenateRows(stringData);  
-        assertEquals(Arrays.asList("100","110"), concRows);
-    }
-    
+ 
     public void testCanGetRowWithMaximumSlice() {
         List<String> intData = (Arrays.asList("1100011","1100110","1011000","0101010","0001100"));
         List<Integer> consRowExpected = new ArrayList<Integer>(Arrays.asList(0,1,2,4));
-        //assertEquals(consRowExpected, Matrix.getMaxSlice(intData));
-        assertEquals(consRowExpected, new Matrix(intData).getMaxSlice());
-        //assertEquals(2, Matrix.fromString("0,1,1\n1,1,1\n0,0,1").getMaxSlice());
+
+        assertEquals(consRowExpected, new Matrix(intData).getRowsWithMaxSlice());
     }
 }
